93f8a18538008dbc9629b603a3b02bb01b7717f5
Michał Karwacki <mic.maci2891@gmail.com>
2025-06-16 00:02:20 +0200

    Add mock tileset to the game

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -18,7 +18,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
+	direction = direction.normalized()
 	
 	pass
 	
